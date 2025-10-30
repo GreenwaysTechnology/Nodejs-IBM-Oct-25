f8d4da5fa65d2eb3d26547a58f5fbc511d7a1f19
subugee <sasubramanian_md@hotmail.com>
2025-10-30 13:02:00 +0530

    expressapp routers

--- expressapp/src/routers/books/index.js
+++ expressapp/src/routers/books/index.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const booksRouter = express.Router()
-const { findAll, save } = require('../../services/books.service')
+const { findAll, save, findById, filterByAuthor, update, remove } = require('../../services/books.service')
 
+const books = [
+    {
+        id: 1, title: 'Node.js Design Patterns', author: 'Mario Casciaro', year: '2014'
+    },
+    {
+        id: 2, title: 'Beginning Node.js, Express & MongoDB Development', author: 'GREG. LIM', year: '2019'
+    }
+]
 //api
 booksRouter.get('/', async (req, res) => {
     const books = await findAll()
     console.log(books)
     res.json(books)
 })
+//api/books/filter?author;
+booksRouter.get('/filter', async (req, res) => {
+    const { author } = req.query
+    const result = await filterByAuthor(author)
+    res.json(result)
+})
+
+//path parameter
+booksRouter.get('/:id', async (req, res) => {
+    const id = +req.params.id
+    const book = await findById(id)
+    if (!book) {
+        res.status(404).json({ message: `Book id ${id} not found` })
+    }
+    res.json(book)
+})
+
 booksRouter.post('/', (req, res) => {
     let book = ''
     req.on('data', (chunk) => {
@@ -18,11 +43,30 @@ booksRouter.post('/', (req, res) => {
         res.status(201).json(newBook)
     })
 })
-booksRouter.put('/', (req, res) => {
-    res.json({ message: 'books put api still in progress' })
+booksRouter.put('/:id', async (req, res) => {
+    let data = ''
+    req.on('data', (chunk) => {
+        data += chunk
+    })
+    req.on('end', async () => {
+        const id = +req.params.id
+        let parsedBook = JSON.parse(data)
+        const updatedBook = await update(id, parsedBook)
+        if (!updatedBook) {
+            res.status(404).json({ error: 'book Not Found' })
+        }
+        res.json(updatedBook)
+    })
+
+
 })
-booksRouter.delete('/', (req, res) => {
-    res.json({ message: 'books delete api still in progress' })
+booksRouter.delete('/:id', async (req, res) => {
+    const id = +req.params.id
+    const response = await remove(id)
+    if (response.length === 0) {
+        res.json({ message: "Book not deleted" })
+    }
+    res.json({ message: 'Book has been deleted succfully' })
 })
 
 
--- expressapp/src/services/books.service.js
+++ expressapp/src/services/books.service.js
@@ -27,5 +27,31 @@ class BookService {
         //return Promise.resolve(newBook)
         return newBook
     }
+    async findById(id) {
+        const book = books.find(tmpBook => tmpBook.id === id)
+        return book
+    }
+    async filterByAuthor(author) {
+        let result = [...books]
+        if (author) {
+            result = result.filter(b => b.author.toLowerCase().includes(author.toLowerCase()))
+        }
+        return result
+    }
+    async update(id, parsedBook) {
+        const book = books.find(b => b.id === id)
+        if (!book) {
+            return null
+        }
+        //update
+        book.author = parsedBook.author
+        book.title = parsedBook.title
+        return book
+    }
+    async remove(id) {
+        return books.filter(b => b.id === id)
+    }
+
+
 }
 module.exports = new BookService()
